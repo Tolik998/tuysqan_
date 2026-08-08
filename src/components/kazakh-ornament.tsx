31d43bf90d440c95ffb40211a268fd98e7f703ef
Tolik998 <tolegentilegen3@gmail.com>
2026-08-09 03:44:42 +0500

    fix: use original page 31 corner ornament

--- src/components/kazakh-ornament.tsx
+++ src/components/kazakh-ornament.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export function KazakhOrnament() {
   return (
     <div
@@ -67,37 +69,16 @@ export function KazakhOrnament() {
 export function KazakhCornerOrnament() {
   return (
     <div
-      className="pointer-events-none absolute -right-5 top-0 hidden h-80 w-80 overflow-hidden text-[#D9B98F] opacity-38 lg:block"
+      className="pointer-events-none absolute right-6 top-0 hidden h-[340px] w-[246px] opacity-55 lg:block xl:right-10 xl:h-[390px] xl:w-[282px]"
       aria-hidden="true"
     >
-      <svg
-        className="h-full w-full"
-        viewBox="0 0 320 320"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M224-20c-20 57 32 87 39 139 8 59-25 103-78 103-38 0-62-25-54-55 7-27 36-39 57-24 19 13 19 42 2 57-12 10-30 8-38-4"
-          stroke="currentColor"
-          strokeWidth="34"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M184 216c-12 41-47 80-94 88-40 7-67-18-58-51 7-27 35-41 58-27 21 13 23 41 6 58-12 12-31 13-43 3"
-          stroke="currentColor"
-          strokeWidth="34"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M187 216c17 50 43 91 88 97 38 5 64-19 56-50-7-27-34-40-56-27-21 13-23 40-7 57 11 12 29 14 42 4"
-          stroke="currentColor"
-          strokeWidth="34"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
+      <Image
+        src="/brand-assets/page31-corner-ornament.svg"
+        alt=""
+        fill
+        sizes="282px"
+        className="object-contain object-right-top"
+      />
     </div>
   );
 }
